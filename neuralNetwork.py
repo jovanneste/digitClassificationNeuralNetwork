83a25b8687d176d707b2020ea71046457a6cdbb2
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-09-15 12:18:47 +0100

    Sigmoid functions added

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -6,4 +6,10 @@ from sklearn.model_selection import train_test_split
 
 dig = load_digits()
 onehot_target = pd.get_dummies(dig.target)
-x_train, x_val, y_train, y_val = train_test_split(dig.data, onehot_target, test_size=0.1, random_state=20)
+x_train, x_test, y_train, y_test = train_test_split(dig.data, onehot_target, test_size=0.1, random_state=20)
+
+def sigmoid(s):
+    return 1/(1+np.exp(-s))
+
+def sigmoid_derv(s):
+    return s*(1-s)
