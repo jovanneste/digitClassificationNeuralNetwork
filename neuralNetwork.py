1ee3c8018b229962887aa197d550102a837d8107
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-09-17 11:52:59 +0100

    Softmax and loss functions added

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -8,8 +8,25 @@ dig = load_digits()
 onehot_target = pd.get_dummies(dig.target)
 x_train, x_test, y_train, y_test = train_test_split(dig.data, onehot_target, test_size=0.1, random_state=20)
 
+#activation functions
 def sigmoid(s):
     return 1/(1+np.exp(-s))
+def softmax(s):
+    exps = np.exp(s - np.max(s, axis=1, keepdims=True))
+    return exps/np.sum(exps, axis=1, keepdims=True)
+
 
 def sigmoid_derv(s):
     return s*(1-s)
+
+
+def cross_entropy(pred, real):
+    n_samples = real.shape[0]
+    res = pred - real
+    return res/n_samples
+
+def loss(pred, real):
+    n_samples = real.shape[0]
+    logp = - np.log(pred[np.arange(n_samples), real.argmax(axis=1)])
+    loss = np.sum(logp)/n_samples
+    return loss
